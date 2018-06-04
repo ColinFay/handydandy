261d62e033e6637a8d176c30d1646286154ffec8
Colin Fay <contact@colinfay.me>
2018-06-04 07:06:12 +0200

    r6 doc

--- R/dandy.R
+++ R/dandy.R
@@ -1,7 +1,38 @@
+#' Handy Dandy
+#'
+#' R6 class for CSS creation
+#'
+#' @section Methods:
+#' \describe{
+#'   \item{\code{styles}}{list of current styles}
+#'   \item{\code{add_style}}{add a style}
+#'   \item{\code{remove_style}}{remove a style}
+#'   \item{\code{update_style}}{update a style}
+#'   \item{\code{swag}}{render as HTML tag}
+#'}
+#'
 #' @importFrom R6 R6Class
 #' @importFrom htmltools HTML
 #'
 #' @export
+#'
+#' @examples
+#' # Create a new dandy
+#' css <- Dandy$new()
+#' # It's easy to add a new style, you need to pass
+#' # + a selector
+#' # + a list of property = value
+#' css$add_style("body", list("font-family" = "Helvetica",
+#'                         "color" = "#24292e"))
+#' css$add_style("h2", list("font-size" = "3 em",
+#'                          "color" = "#911414",
+#'                          "text-align" = "center"))
+#' # Easy to update
+#' css$update_style("body","color","#911414")
+#' # Or to remove one
+#' css$remove_style("body")
+#' # You can also simply remove one part of your tag
+#' css$remove_style("h2", "color")
 
 Dandy <- R6::R6Class("Dandy",
                    public = list(
